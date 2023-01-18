26db1d34dcccc4dad02a2c4d52a192d495c5d41e
Pavel Steapents <pahhan.ne@gmail.com>
2023-01-18 14:13:57 +0700

    fix channel cancel when connection closed

---
+++ recipients/cancelable_amqp_recipient_test.go
@@ -0,0 +1,45 @@
+package recipients
+
+import (
+	"context"
+	"testing"
+)
+
+func TestNewDefaultCancelableAmqpRecipient(t *testing.T) {
+	r := NewDefaultCancelableAmqpRecipient(
+		context.Background(),
+		"foo",
+		8,
+		nil,
+		nil,
+		nil,
+	)
+
+	if "foo" != r.queue {
+		t.Errorf("queue name expected to be 'foo', '%s' given", r.queue)
+	}
+
+	if 8 != r.prefetch {
+		t.Errorf("prefetch expected to be 8, '%d' given", r.prefetch)
+	}
+
+	if nil != r.consumeOptions.Args {
+		t.Error("Options Args expected to be null")
+	}
+
+	if r.consumeOptions.NoWait {
+		t.Error("Options NoWait expected to be false")
+	}
+
+	if r.consumeOptions.NoLocal {
+		t.Error("Options NoLocal expected to be false")
+	}
+
+	if r.consumeOptions.Exclusive {
+		t.Error("Options Exclusive expected to be false")
+	}
+
+	if r.consumeOptions.AutoAck {
+		t.Error("Options AutoAck expected to be false")
+	}
+}
